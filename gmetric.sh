9041a7eb193d0926e3eefc524dbb31571cff460c
Krishna Bagal <krishna.bagal@gmail.com>
2018-04-16 10:29:23 +0530

    Updated some changes.

--- gmetric.sh
+++ gmetric.sh
@@ -2,14 +2,18 @@
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
 #Script Name: gmetric.sh
 #Date: Sep 06th, 2017. 
-#Modified: NA
-#Versioning: NA
+#Modified: Apr 16th, 2017
+#Versioning: 0.1 = UPdated hostip, username and password as veriable.
 #Author: Krishna Bagal.
 #Info: Tomcat thread and memory information/Value send to ganglia server.
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
 value='^[0-9]+([.][0-9]+)?$'
-T8080=`check_tomcat.py -H <Server IP Address> -p 8080 -u <tomcat-manager-user> -a <tomcat-manager-passsword> -m thread -w 999 -c 1000 -U "/manager" -C \"http-bio-8080\" |awk {'print $3'}`
-M8080=`check_tomcat.py -H <Server IP Address> -p 8080 -u <tomcat-manager-user> -a <tomcat-manager-passsword> -m mem -w 90 -c 99 | cut -d= -f2 |cut -d% -f1`
+hostip=`ip route get 1 | awk '{print $NF;exit}'`
+user="USERNAME"
+password="PASSWORD"
+
+T8080=`check_tomcat.py -H $hostip -p 8080 -u $user -a $password -m thread -w 999 -c 1000 -U "/manager" -C \"http-bio-8080\" |awk {'print $3'}`
+M8080=`check_tomcat.py -H $hostip -p 8080 -u $user -a $password -m mem -w 90 -c 99 | cut -d= -f2 |cut -d% -f1`
 if ! [[ $T8080 =~ $value ]] ; 
 then
            /usr/bin/gmetric --name  Tomcat-Thread-8080 --value 0 --type int32 --unit Count
